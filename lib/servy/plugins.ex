d98c13bd9e3b7b4cefea2a09f4f487651939771b
victor hazbun <victorhazbun87@gmail.com>
2018-04-05 17:13:45 -0500

    Modeling With Structs

--- lib/servy/plugins.ex
+++ lib/servy/plugins.ex
@@ -1,19 +1,21 @@
 require Logger
 
 defmodule Servy.Plugins do
+  alias Servy.Conv
+
   @doc "Logs 404 requests"
-  def track(%{status: 404, path: path} = conv) do
+  def track(%Conv{status: 404, path: path} = conv) do
     Logger.warn("Warning: #{path} is on the loose!")
     conv
   end
 
-  def track(conv), do: conv
+  def track(%Conv{} = conv), do: conv
 
-  def rewrite_path(%{path: "/wildlife"} = conv) do
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
     %{conv | path: "/wildthings"}
   end
 
-  def rewrite_path(%{path: path} = conv) do
+  def rewrite_path(%Conv{path: path} = conv) do
     regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
     captures = Regex.named_captures(regex, path)
     rewrite_path_captures(conv, captures)
@@ -23,7 +25,7 @@ defmodule Servy.Plugins do
     %{conv | path: "/#{thing}/#{id}"}
   end
 
-  def rewrite_path_captures(conv, nil), do: conv
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
 
-  def log(conv), do: IO.inspect(conv)
+  def log(%Conv{} = conv), do: IO.inspect(conv)
 end
