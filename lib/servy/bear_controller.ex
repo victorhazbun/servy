5dd82f7ebafce4463b8b8a7910e0cf9eb8144ed3
victor hazbun <victorhazbun87@gmail.com>
2018-05-13 15:02:04 +1000

    Comprehensions

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -3,28 +3,23 @@ defmodule Servy.BearController do
   alias Servy.Wildthings
   alias Servy.Bear
 
-  defp bear_item(bear) do
-    "<li>#{bear.name} - #{bear.type}</li>"
-  end
+  @templates_path Path.expand("../../templates", __DIR__)
 
   def index(conv) do
-    items =
+    bears =
       Wildthings.list_bears()
-      |> Enum.filter(&Bear.is_grizzly/1)
       |> Enum.sort(&Bear.order_asc_by_name/2)
-      |> Enum.map(&bear_item/1)
-      |> Enum.join()
 
-    %Conv{conv | status: 200, resp_body: "<ul>#{items}</ul>"}
+    render(conv, "index.eex", bears: bears)
   end
 
   def show(conv, %{"id" => id}) do
     bear = Wildthings.get_bear(id)
 
-    %Conv{conv | status: 200, resp_body: "<h1>#{bear.id} - #{bear.name}</h1>"}
+    render(conv, "show.eex", bear: bear)
   end
 
-  def create(conv, %{"name" => name, "type" => type} = params) do
+  def create(conv, %{"name" => name, "type" => type} = _params) do
     %Conv{
       conv
       | status: 201,
@@ -34,5 +29,14 @@ defmodule Servy.BearController do
 
   def delete(conv, _params) do
     %Conv{conv | status: 403, resp_body: "Deleting a bear is forbidden!"}
+  end
+
+  defp render(conv, template, bindings \\ []) do
+    content =
+      @templates_path
+      |> Path.join(template)
+      |> EEx.eval_file(bindings)
+
+    %Conv{conv | status: 200, resp_body: content}
   end
 end
