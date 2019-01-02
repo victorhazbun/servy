ee02571524bddce1e65217836fa496df8a4402c2
victor hazbun <victorhazbun87@gmail.com>
2019-01-02 20:32:36 +1100

    Change HttpServerTest to Send Concurrent Requests

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -3,12 +3,31 @@ defmodule HttpServerTest do
 
 	alias Servy.HttpServer
 
-	test "accepts a request on a socket and sends back a response" do
-		spawn(HttpServer, :start, [4000])
+test "accepts a request on a socket and sends back a response" do
+  spawn(HttpServer, :start, [4000])
 
-		{:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+  parent = self()
 
-		assert response.status_code == 200
-		assert response.body == "Bears, Lions, Tigers"
-	end
+  max_concurrent_requests = 5
+
+  # Spawn the client processes
+  for _ <- 1..max_concurrent_requests do
+    spawn(fn ->
+      # Send the request
+      {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+      # Send the response back to the parent
+      send(parent, {:ok, response})
+    end)
+  end
+
+  # Await all {:handled, response} messages from spawned processes.
+  for _ <- 1..max_concurrent_requests do
+    receive do 
+      {:ok, response} -> 
+        assert response.status_code == 200
+        assert response.body == "Bears, Lions, Tigers"
+    end
+  end
+end
 end
