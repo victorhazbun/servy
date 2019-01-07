c2ce0fdf2c782594fcd221e104c8f371018dece0
victor hazbun <victorhazbun87@gmail.com>
2019-01-07 13:53:17 +1100

    Change HttpServerTest to Check Multiple URLs

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -6,17 +6,21 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
 
-    url = "http://localhost:4000/wildthings"
+    urls = [
+      "http://localhost:4000/wildthings",
+      "http://localhost:4000/bears",
+      "http://localhost:4000/bears/1",
+      "http://localhost:4000/wildlife",
+      "http://localhost:4000/api/bears"
+    ]
 
-    1..5
-    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end ) end)
-    |> IO.inspect(label: "before")
+    urls
+    |> Enum.map(&Task.async(fn -> HTTPoison.get(&1) end))
     |> Enum.map(&Task.await/1)
     |> Enum.map(&assert_successful_response/1)
   end
 
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
-    assert response.body == "Bears, Lions, Tigers"
   end
 end
