55633747686e353959ac644768eb525f1408cfad
victor hazbun <victorhazbun87@gmail.com>
2019-01-07 12:08:52 +1100

    Asynchronous Tasks

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -5,6 +5,7 @@ defmodule Servy.Handler do
   alias Servy.Conv
   alias Servy.BearController
   alias Servy.VideoCam
+  alias Servy.Tracker
     
   @pages_path Path.expand("pages", File.cwd!())
 
@@ -24,20 +25,17 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def route(%Conv{ method: "GET", path: "/snapshots"} = conv) do
-    parent = self() # This is the request-handing process
-    
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
-    
-    snapshot1 = receive do {:result, filename} -> filename end
-    snapshot2 = receive do {:result, filename} -> filename end
-    snapshot3 = receive do {:result, filename} -> filename end
+  def route(%Conv{ method: "GET", path: "/sensors"} = conv) do
+    task = Task.async(fn -> Tracker.get_location("bigfoot") end)
 
-    snapshots = [snapshot1, snapshot2, snapshot3]
+    snapshots = 
+    ["cam-1", "cam-2", "cam-3"]
+    |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+    |> Enum.map(&Task.await/1)
+    
+    where_is_bigfoot = Task.await(task)
 
-    %{ conv | status: 200, resp_body: inspect snapshots }
+    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
 
   def route(%Conv{ method: "GET", path: "/kaboom" }) do
