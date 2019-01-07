8c061662217fc01358681ffc7fb108ca6b69d792
victor hazbun <victorhazbun87@gmail.com>
2019-01-07 13:46:47 +1100

    Change HttpServerTest to Use Tasks

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -3,31 +3,20 @@ defmodule HttpServerTest do
 
 	alias Servy.HttpServer
 
-test "accepts a request on a socket and sends back a response" do
-  spawn(HttpServer, :start, [4000])
+  test "accepts a request on a socket and sends back a response" do
+    spawn(HttpServer, :start, [4000])
 
-  parent = self()
+    url = "http://localhost:4000/wildthings"
 
-  max_concurrent_requests = 5
-
-  # Spawn the client processes
-  for _ <- 1..max_concurrent_requests do
-    spawn(fn ->
-      # Send the request
-      {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
-
-      # Send the response back to the parent
-      send(parent, {:ok, response})
-    end)
+    1..5
+    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end ) end)
+    |> IO.inspect(label: "before")
+    |> Enum.map(&Task.await/1)
+    |> Enum.map(&assert_successful_response/1)
   end
 
-  # Await all {:handled, response} messages from spawned processes.
-  for _ <- 1..max_concurrent_requests do
-    receive do 
-      {:ok, response} -> 
-        assert response.status_code == 200
-        assert response.body == "Bears, Lions, Tigers"
-    end
+  defp assert_successful_response({:ok, response}) do
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
   end
-end
 end
